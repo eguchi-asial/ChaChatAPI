560d09596e016a123d4a927d8d1fcec070bd3315
eguchi <eguchi@asial.co.jp>
2020-09-19 16:28:28 +0900

    socket-ioの導入とprettierの導入

--- src/server.ts
+++ src/server.ts
@@ -1,44 +1,26 @@
-"use strict";
+'use strict';
 
-import express from "express";
-import { v4 as uuidv4 } from "uuid";
+import express from 'express';
+import http from 'http';
+import socketio from 'socket.io';
+import { v4 as uuidv4 } from 'uuid';
 
 const app: express.Express = express();
+const server: http.Server = http.createServer(app);
+const io: socketio.Server = socketio(server);
 
-// body-parserに基づいた着信リクエストの解析
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const PORT = process.env.PORT || 3000;
 
-// CORSの許可
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:8080");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
+// http API
+app.get('/', function (req, res) {
+  res.send(uuidv4());
 });
 
-// GetとPostのルーティング
-const router: express.Router = express.Router();
-
-router.get("/", (req: express.Request, res: express.Response) => {
-  res.send({
-    message: "OK",
-  });
+// ws API
+io.on('connection', function (socket) {
+  console.log('connected');
 });
 
-router.post("/", (req: express.Request, res: express.Response) => {
-  const userId: string = req.body.userId;
-  const crypto = require("crypto");
-  const md5 = crypto.createHash("md5");
-  const hasheduserId: string = md5.update(userId, "binary").digest("hex");
-  res.send({
-    uuid: `${hasheduserId}.${uuidv4()}`,
-  });
-});
-
-app.use(router);
-app.listen(3000, () => {
-  console.log("ChachatApi listening on port 3000!");
+server.listen(PORT, function () {
+  console.log('server listening. Port:' + PORT);
 });
