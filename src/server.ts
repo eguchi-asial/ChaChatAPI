883579c18d2902c425f6ebf239f02917ec6bf40d
eguchi <eguchi@asial.co.jp>
2020-09-22 09:12:45 +0900

    フロント連携

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,9 @@
 
 import express from 'express';
 import http from 'http';
-import socketio from 'socket.io';
+import socketio, { Socket } from 'socket.io';
 import { v4 as uuidv4 } from 'uuid';
+import Chat from './types/chat';
 
 const app: express.Express = express();
 const server: http.Server = http.createServer(app);
@@ -11,16 +12,34 @@ const io: socketio.Server = socketio(server);
 
 const PORT = process.env.PORT || 3000;
 
+// CORSの許可
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
 // http API
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.send(uuidv4());
 });
 
 // ws API
-io.on('connection', function (socket) {
+io.on('connection', (socket: Socket) => {
   console.log('connected');
+
+  socket.on('post-message', (msg: Chat) => {
+    console.log('post-message', msg);
+    io.emit('receive-message', {
+      ...msg,
+      postId: msg?.postId || uuidv4(),
+    });
+  });
 });
 
-server.listen(PORT, function () {
+server.listen(PORT, () => {
   console.log('server listening. Port:' + PORT);
 });
---
+++ src/types/chat.ts
@@ -0,0 +1,6 @@
+export default interface Chat {
+  name?: string | null;
+  body: string;
+  postId: string | null;
+  postedAt: string;
+}
