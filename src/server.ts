88c8c0a7b939bc14faa48e833dc4c67acac7c82a
eguchi <eguchi@asial.co.jp>
2020-09-26 15:50:29 +0900

    IDをMD5に

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,8 @@ import http from 'http';
 import socketio, { Socket } from 'socket.io';
 import { v4 as uuidv4 } from 'uuid';
 import Chat from './types/chat';
+import { MD5, enc } from 'crypto-js';
+import moment from 'moment';
 
 const app: express.Express = express();
 const server: http.Server = http.createServer(app);
@@ -34,15 +36,31 @@ io.on('connection', (socket: Socket) => {
     socket.request.connection.remoteAddress;
   console.log('connected', clientIpAddress);
   /* クライアントからのメッセージ受信処理 */
-  socket.on('post-message', (msg: Chat) => {
+  socket.on('post-message', async (msg: Chat) => {
     /* 受信したメッセージをルームメンバーに通知pushする */
     io.emit('receive-message', {
       ...msg,
-      postId: msg?.postId || clientIpAddress,
+      postId: msg?.postId || (await digestMessage(clientIpAddress)),
     });
+  });
+  /* disconnected */
+  socket.on('disconnect', (reason: string) => {
+    console.log(`disconnect: ${reason}`);
   });
 });
 
 server.listen(PORT, () => {
   console.log('server listening. Port:' + PORT);
 });
+
+/**
+ * ハッシュ関数
+ * セキュリティ用途なし
+ * https://developer.mozilla.org/ja/docs/Web/API/SubtleCrypto/digest
+ * @param message string ハッシュ化したい文字列
+ */
+async function digestMessage(message: string) {
+  return MD5(`${message}-${moment().format('YYYY-MM-DD')}`).toString(
+    enc.Base64
+  );
+}
