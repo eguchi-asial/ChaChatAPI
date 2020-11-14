c3f57f18792c8430ad5b41ea1550ba83bd3c04eb
eguchi <eguchi@asial.co.jp>
2020-11-14 15:32:27 +0900

    code deployのAfterInstallのshでdocker-composeがNFだったのでlnしておく

--- aws/ec2.tf
+++ aws/ec2.tf
@@ -24,6 +24,7 @@ usermod -aG docker $USER
 docker info
 curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 docker-compose --version
 # codedeploy
 yum install -y ruby
